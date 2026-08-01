3c2a1b5ac192ee01044af7e02a8a5e5b029cc156
benbunsford <benbridgford@gmail.com>
2026-08-01 13:46:31 -0700

    feat: add placeholder ebitengine game

---
+++ cmd/game/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
+	"log"
+)
+
+// Game implements ebiten.Game interface.
+type Game struct{}
+
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
+func (g *Game) Update() error {
+	// Write your game's logical update.
+	return nil
+}
+
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Write your game's rendering.
+
+	screen.Fill(color.RGBA{255, 255, 255, 255})
+
+	ebitenutil.DebugPrint(screen, "Weeeeeeeee")
+}
+
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return ebiten.WindowSize()
+}
+
+func main() {
+	game := &Game{}
+	// Specify the window size as you like. Here, a doubled size is specified.
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Baby Yayga")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	// Call ebiten.RunGame to start your game loop.
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
