1d5abc7852d3616eb0ff05d507f23ca03a0bb5aa
benbunsford <benbridgford@gmail.com>
2026-08-11 15:34:00 -0700

    feat: build out protocol package, add types and marshal funcs

--- internal/protocol/message.go
+++ internal/protocol/message.go
@@ -1,13 +1,54 @@
 package protocol
 
 import (
-// "github.com/ben-rw/webgame/internal/room"
+	"encoding/json"
+	"errors"
 )
 
-type JoinMessage struct {
-	RoomID string
+// "github.com/ben-rw/webgame/internal/room"
+
+type MessageType string
+
+const RoomState MessageType = "RoomState"
+
+type Message struct {
+	Type MessageType
+	Data json.RawMessage
 }
 
-type JoinResponse struct {
-	Username string
+type RoomStateData struct {
+	Username string `json:"username"`
+	RoomID   string `json:"room_id"`
+}
+
+func (m *Message) UnmarshalMessageData() (any, error) {
+	var v any
+
+	switch m.Type {
+	case RoomState:
+		v = &RoomStateData{}
+	default:
+		return nil, errors.New("unrecognized message format")
+	}
+
+	err := json.Unmarshal(m.Data, v)
+	if err != nil {
+		return nil, err
+	}
+
+	return v, nil
+}
+
+func MarshalToMessage(mt MessageType, msgData any) (*Message, error) {
+	jsonData, err := json.Marshal(msgData)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := &Message{
+		Type: mt,
+		Data: jsonData,
+	}
+
+	return msg, nil
 }
