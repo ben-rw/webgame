0c271ecff07777f0cdf4eddd6878f46e3298e8cd
benbunsford <benbridgford@gmail.com>
2026-08-11 18:03:37 -0700

    feat: add client websocket logic

--- cmd/game/internal/ws/websocket.go
+++ cmd/game/internal/ws/websocket.go
@@ -4,54 +4,108 @@ import (
 	"context"
 	"log"
 	"syscall/js"
-	"time"
 
 	"github.com/ben-rw/webgame/internal/protocol"
 	"github.com/coder/websocket"
 	"github.com/coder/websocket/wsjson"
 )
 
-func connectToWebsocket() error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
+type Connection struct {
+	*websocket.Conn
+	messages chan protocol.Message
+}
 
-	prot, host, roomID, username := getClientInfo()
+// check for messages to pass to Update in main
+func (c *Connection) Check() []protocol.Message {
+	msgs := []protocol.Message{}
+	for {
+		select {
+		case msg := <-c.messages:
+			msgs = append(msgs, msg)
+		default:
+			return msgs
+		}
+	}
+}
 
+// read incoming messages, close dead connections, drop messages if buffer fills up
+func (c *Connection) readLoop() {
+	defer c.Close(websocket.StatusInternalError, "connection closed unexpectedly")
+
+	msg := protocol.Message{}
+
+	for {
+		err := wsjson.Read(context.Background(), c.Conn, &msg)
+		if err != nil {
+			if websocket.CloseStatus(err) == websocket.StatusNormalClosure ||
+				websocket.CloseStatus(err) == websocket.StatusGoingAway {
+				c.Close(websocket.StatusNormalClosure, "connection closed normally")
+				return
+			} else {
+				log.Printf("wsjson read error: %v\n", err)
+				log.Println("terminating read loop")
+				return
+			}
+		}
+		select {
+		case c.messages <- msg:
+			log.Printf("received msg: %v", msg)
+		default:
+			log.Println("buffer full: dropped a message")
+		}
+	}
+
+}
+
+// set up websocket connection, send roomID to server, start read loop
+func ConnectToWebsocket() (*Connection, error) {
+	ctx := context.Background()
+
+	prot, host, roomID := getClientInfo()
 	wsURL := prot + host + "/room/" + roomID + "/ws"
-
-	log.Println(wsURL, roomID, username)
 
 	conn, _, err := websocket.Dial(ctx, wsURL, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer conn.CloseNow()
 
-	msg := protocol.JoinMessage{
+	joinData := protocol.JoinRequestData{
 		RoomID: roomID,
+	}
+
+	msg, err := protocol.MarshalToMessage(protocol.JoinRequest, joinData)
+	if err != nil {
+		return nil, err
 	}
 
 	err = wsjson.Write(ctx, conn, msg)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	msgs := make(chan protocol.Message, 20)
+	c := &Connection{
+		conn,
+		msgs,
+	}
+
+	go c.readLoop()
+
+	return c, nil
 }
 
-// returns websocketURL, roomID, username
-func getClientInfo() (string, string, string, string) {
+// use syscall/js to get url info and roomID
+func getClientInfo() (string, string, string) {
 	loc := js.Global().Get("location")
 
-	protocol := "ws://"
+	prot := "ws://"
 	if loc.Get("protocol").String() == "https:" {
-		protocol = "wss://"
+		prot = "wss://"
 	}
 	host := loc.Get("host").String()
 
 	params := js.Global().Get("URLSearchParams").New(loc.Get("search"))
 	roomID := params.Call("get", "room").String()
-	username := params.Call("get", "name").String()
 
-	return protocol, host, roomID, username
+	return prot, host, roomID
 }
