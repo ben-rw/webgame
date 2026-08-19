c092aa3685a3a2396c00b7b8628048b4dbf0df35
benbunsford <benbridgford@gmail.com>
2026-08-18 18:42:09 -0700

    refactor: make assets available to all scenes

--- cmd/game/internal/assets/embed.go
+++ cmd/game/internal/assets/embed.go
@@ -1,4 +1,4 @@
-package lobby
+package assets
 
 import (
 	"embed"
--- cmd/game/internal/scenes/lobby/types.go
+++ cmd/game/internal/scenes/lobby/types.go
@@ -1,6 +1,7 @@
 package lobby
 
 import (
+	"github.com/ben-rw/webgame/cmd/game/internal/assets"
 	"github.com/ben-rw/webgame/internal/protocol"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -20,7 +21,7 @@ type Sprite struct {
 func NewPlayer(data *protocol.PlayerData, joinOrder int) *Player {
 	log.Printf("playerdata sprite index: %v", data.SpriteIndex)
 	imgPath := PlayerSpriteIndex[data.SpriteIndex]
-	playerImg, _, err := ebitenutil.NewImageFromFileSystem(AssetsFS, imgPath)
+	playerImg, _, err := ebitenutil.NewImageFromFileSystem(assets.AssetsFS, imgPath)
 	if err != nil {
 		log.Fatal(err)
 	}
