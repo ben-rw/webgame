ddeb69daf840dae027f11e7d0c2fc9e607ad18cb
benbunsford <benbridgford@gmail.com>
2026-08-18 18:19:24 -0700

    feat: add playerupdate to update only the main player

--- cmd/game/internal/scenes/lobby/lobby.go
+++ cmd/game/internal/scenes/lobby/lobby.go
@@ -1,8 +1,10 @@
 package lobby
 
 import (
+	"github.com/ben-rw/webgame/cmd/game/internal/ws"
 	"github.com/ben-rw/webgame/internal/protocol"
 	"github.com/hajimehoshi/ebiten/v2"
+
 	//	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"image"
 	"image/color"
@@ -10,16 +12,18 @@ import (
 )
 
 type Lobby struct {
+	Conn    *ws.Connection
 	Players map[string]*Player
 	Player  *Player
 	Sprites []*Sprite
 }
 
-func New() *Lobby {
+func New(c *ws.Connection) *Lobby {
 
 	return &Lobby{
+		Conn:    c,
 		Players: map[string]*Player{},
-		Player:  NewPlayer(&protocol.PlayerData{}),
+		Player:  NewPlayer(&protocol.PlayerData{}, 0),
 		Sprites: []*Sprite{},
 	}
 }
@@ -43,29 +47,48 @@ func (l *Lobby) Update(messages []protocol.Message) error {
 				if _, ok := l.Players[playerData.Name]; ok {
 					l.Players[playerData.Name].Data = playerData
 				} else {
-					player := NewPlayer(playerData)
+					player := NewPlayer(playerData, len(l.Players))
 					l.Players[playerData.Name] = player
 				}
 			}
 
 			l.Player = l.Players[data.PlayerData.Name]
 
-		case protocol.SceneChange:
+		case protocol.PlayerUpdate:
+			playerUpdateData, err := message.UnmarshalMessageData()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			data, ok := playerUpdateData.(*protocol.PlayerUpdateData)
+			if !ok {
+				log.Println("failed type assertion")
+				continue
+			}
+			l.Player = l.Players[data.PlayerData.Name]
+
+		default:
 		}
 	}
 
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		l.Player.X += 2
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		l.Player.X -= 2
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		l.Player.Y -= 2
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		l.Player.Y += 2
-	}
+	// if ebiten.IsKeyPressed(ebiten.KeyRight) {
+	// 	l.Player.X += 2
+	// }
+	// if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+	// 	l.Player.X -= 2
+	// }
+	// if ebiten.IsKeyPressed(ebiten.KeyUp) {
+	// 	l.Player.Y -= 2
+	// }
+	// if ebiten.IsKeyPressed(ebiten.KeyDown) {
+	// 	l.Player.Y += 2
+	// }
+
+	// playerUpdate := &protocol.PlayerUpdateData{
+	// PlayerData: l.Player.Data,
+	// }
+	// l.Conn.WriteMsg(protocol.PlayerUpdate, playerUpdate)
+
 	return nil
 }
 
