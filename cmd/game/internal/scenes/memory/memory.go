48fe0cdbb4c181a1ab1e8f00538dfd242825ac3e
benbunsford <benbridgford@gmail.com>
2026-08-18 18:44:50 -0700

    test: add dummy memory scene to test scene changing

---
+++ cmd/game/internal/scenes/memory/memory.go
@@ -0,0 +1,30 @@
+package memory
+
+import (
+	"github.com/ben-rw/webgame/cmd/game/internal/ws"
+	"github.com/ben-rw/webgame/internal/protocol"
+	"github.com/hajimehoshi/ebiten/v2"
+	//	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+)
+
+type Memory struct {
+	Conn *ws.Connection
+	// Players map[string]*Player
+	// Player  *Player
+	// Sprites []*Sprite
+}
+
+func New(c *ws.Connection) *Memory {
+	log.Println("scene changed to Memory")
+	return &Memory{
+		Conn: c,
+		// Players: map[string]*Player{},
+		// Player:  NewPlayer(&protocol.PlayerData{}, 0),
+		// Sprites: []*Sprite{},
+	}
+}
+
+func (m *Memory) Update(messages []protocol.Message) error { return nil }
+
+func (l *Memory) Draw(screen *ebiten.Image) {}
